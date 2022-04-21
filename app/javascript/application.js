e3674b600c431692e7684f0bd17975660b55e823
Lance Foley <lancefoley@Lances-MBP.lan>
2022-04-21 15:15:00 -0500

    chartkick

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-
+import "chartkick"
 import Highcharts from "highcharts"
 
 window.Highcharts = Highcharts

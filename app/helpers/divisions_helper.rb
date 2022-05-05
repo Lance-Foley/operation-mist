423481edaa0ac31e6dcb445592e03f8b8a78c17e
Lance Foley <lancefoley@Lances-MBP.lan>
2022-05-05 13:04:58 -0500

    Added Autmatic Calulations to Divisions phases crews

--- app/helpers/divisions_helper.rb
+++ app/helpers/divisions_helper.rb
@@ -1,3 +1,11 @@
 module DivisionsHelper
-  
+
+  def work_load
+    @workload = Job.where(division: @name).sum(:three_month_hours) / Employee.where(division: @name).sum(:man_hours_three_months)
+    if @workload < 0
+      0.0
+    else
+      @workload
+    end
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,11 +4,11 @@ class Employee < ApplicationRecord
   private def create_or_update(touch: nil, **)
 
     self.true_man_hours_per_week = (man_hours_per_week * 0.71).round(1)
-    self.man_hours_per_month = (true_man_hours_per_week * 4.3).round(1)
-    self.man_hours_three_months = (man_hours_per_month * 3).round(1)
-    self.man_hours_six_months = (man_hours_per_month * 6).round(1)
-    self.man_hours_nine_months = (man_hours_per_month * 9).round(1)
-    self.man_hours_twelve_months = (man_hours_per_month * 12).round(1)
+    self.man_hours_per_month = (true_man_hours_per_week * 4.3)
+    self.man_hours_three_months = (man_hours_per_month * 3)
+    self.man_hours_six_months = (man_hours_per_month * 6)
+    self.man_hours_nine_months = (man_hours_per_month * 9)
+    self.man_hours_twelve_months = (man_hours_per_month * 12)
 
     super
   end

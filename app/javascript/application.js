65ccbb89ef848dcf3fa620ea0979c722d0ec53e0
lance_MacBook_Pro <foleylance93@gmail.com>
2022-05-24 11:58:07 -0500

    dashboard helperfix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //
 import "@hotwired/turbo-rails"
+import "controllers"
 import "chartkick"
 import Highcharts from "highcharts"
-import "./controllers"
 
 
 window.Highcharts = Highcharts

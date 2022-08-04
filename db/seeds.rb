505d0ff88777a7671a8f50067d20421109ecfddd
lance_MacBook_Pro <foleylance93@gmail.com>
2022-08-04 07:16:07 -0500

    UI Upgrades Removed Future address from client model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Variance.create(hour_variance: 0.71)
+Variance.create(hour_variance: 0.71, hour_rate: 40)
 r1 = Role.create({ name: 'Sales', description: 'Can read items' })
 r2 = Role.create({ name: 'Manager', description: 'Can read and create items. Can update and destroy own items' })
 r3 = Role.create({ name: 'Admin', description: 'Can perform any CRUD operation on any resource' })
 
-u1 = User.create({ name: 'Admin', email: 'Admin', password: 'admin', password_confirmation: 'admin', role_id: r3.id })
+u1 = User.create({ name: 'admin', email: 'admin@example.com', password: 'admin', password_confirmation: 'admin', role_id: r3.id })
